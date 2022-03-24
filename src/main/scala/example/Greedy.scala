5c6aca36ef492481c4b30bc2895c8e0de6d638b3
yordwynn <yurij.a.egorov@gmail.com>
2022-03-25 01:51:19 +0500

    881. Boats to Save People

--- src/main/scala/example/Greedy.scala
+++ src/main/scala/example/Greedy.scala
@@ -1,6 +1,11 @@
 package example
 
 object Greedy {
+  def main(args: Array[String]): Unit = {
+    val people = Array(5, 1, 7, 4, 2, 4)
+    print(numRescueBoats(people, 7))
+  }
+
   def brokenCalc(startValue: Int, target: Int): Int = {
     var res = 0
     var current = target
@@ -15,5 +20,35 @@ object Greedy {
     }
 
     res + (startValue - current)
+  }
+
+  def numRescueBoats(people: Array[Int], limit: Int): Int = {
+    val sortedPeople = people.sorted(Ordering.Int.reverse)
+    var res = 0
+    var p = 0
+
+    while (p < people.length) {
+      if (sortedPeople(p) > -1) {
+        val first = sortedPeople(p)
+
+        var p2 = p + 1
+        while (
+          p2 < sortedPeople.length && ((sortedPeople(
+            p2
+          ) == -1) || first + sortedPeople(p2) > limit)
+        )
+          p2 = p2 + 1
+
+        if (p2 == sortedPeople.length) {
+          res = res + 1
+        } else {
+          res = res + 1
+          sortedPeople(p2) = -1
+        }
+      }
+      p = p + 1
+    }
+
+    res
   }
 }
