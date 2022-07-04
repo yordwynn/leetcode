398aa7b9d09006b5afda10611a84b2eeeb07fc4d
yordwynn <yurij.a.egorov@gmail.com>
2022-07-05 00:35:10 +0500

    135. Candy fix

--- src/main/scala/candy/Solution.scala
+++ src/main/scala/candy/Solution.scala
@@ -4,34 +4,40 @@ import scala.annotation.tailrec
 
 object Solution {
   def candy(ratings: Array[Int]): Int = {
-    val cnds = Array.fill(ratings.length)(1)
+    val left2right = Array.fill(ratings.length)(1)
+    val right2left = Array.fill(ratings.length)(1)
 
     @tailrec
-    def go(i: Int = 1): Array[Int] = {
-      if (i >= cnds.length)
-        cnds
+    def l2r(i: Int): Array[Int] = {
+      if (i >= left2right.length)
+        left2right
       else if (ratings(i) > ratings(i - 1)) {
-        cnds(i) = cnds(i - 1) + 1
-        go(i + 1)
-      } else if (ratings(i) < ratings(i - 1) && cnds(i) >= cnds(i - 1)) {
-        giveExtra(i - 1)
-        go(i + 1)
+        left2right(i) = left2right(i - 1) + 1
+        l2r(i + 1)
       } else
-        go(i + 1)
+        l2r(i + 1)
     }
 
     @tailrec
-    def giveExtra(i: Int): Array[Int] = {
-      cnds(i) = cnds(i) + 1
-      if (i == 0)
-        cnds
-      else if (cnds(i) >= cnds(i - 1) && ratings(i) < ratings(i - 1))
-        giveExtra(i - 1)
-      else
-        cnds
+    def r2l(i: Int): Array[Int] = {
+      if (i < 0)
+        right2left
+      else if (ratings(i) > ratings(i + 1)) {
+        right2left(i) = right2left(i + 1) + 1
+        r2l(i - 1)
+      } else
+        r2l(i - 1)
     }
 
-    go()
-    cnds.sum
+    val l = l2r(1)
+    val r = r2l(ratings.length - 2)
+
+    l.zip(r)
+      .foldLeft(0)((res, item) =>
+        item match {
+          case (x, y) if x >= y => res + x
+          case (x, y) if y > x  => res + y
+        }
+      )
   }
 }
