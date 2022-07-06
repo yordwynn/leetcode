d0c8517beafda382822e9ace287c0b778876f66d
yordwynn <yurij.a.egorov@gmail.com>
2022-07-07 02:04:32 +0500

    509. Fibonacci Number

---
+++ src/main/scala/fibonacciNumber/Solution.scala
@@ -0,0 +1,17 @@
+package fibonacciNumber
+
+import scala.annotation.tailrec
+
+object Solution {
+  def fib(n: Int): Int = {
+    @tailrec
+    def go(f1: Int, f2: Int, i: Int): Int = {
+      if (i == 0)
+        f1
+      else
+        go(f2, f1 + f2, i - 1)
+    }
+
+    go(0, 1, n)
+  }
+}
---
+++ src/test/scala/fibonacciNumber/SolutionTest.scala
@@ -0,0 +1,17 @@
+package fibonacciNumber
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class SolutionTest extends AnyFlatSpec {
+  "example 1" should "return 1" in {
+    val n = 2
+
+    assert(Solution.fib(n) == 1)
+  }
+
+  "example 2" should "return 2" in {
+    val n = 3
+
+    assert(Solution.fib(n) == 2)
+  }
+}
