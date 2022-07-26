df3ee16b2c40b955c8483d3bbde7b5c8062e2821
yordwynn <yurij.a.egorov@gmail.com>
2022-07-27 02:09:48 +0500

    236. Lowest Common Ancestor of a Binary Tree

---
+++ src/main/scala/lowestCommonAncestorOfABinaryTree/Solution.scala
@@ -0,0 +1,35 @@
+package lowestCommonAncestorOfABinaryTree
+
+import example.TreeNode
+
+// 236. Lowest Common Ancestor of a Binary Tree
+object Solution {
+  def lowestCommonAncestor(
+      root: TreeNode,
+      p: TreeNode,
+      q: TreeNode
+  ): TreeNode = {
+    def findNode(
+        node: TreeNode,
+        target: TreeNode,
+        res: Seq[TreeNode]
+    ): (Boolean, Seq[TreeNode]) = {
+      if (node == null)
+        (false, res)
+      else if (node.value == target.value)
+        (true, node +: res)
+      else {
+        val inLeft = findNode(node.left, target, node +: res)
+        if (inLeft._1)
+          inLeft
+        else
+          findNode(node.right, target, node +: res)
+      }
+    }
+
+    val pPath = findNode(root, p, Nil)._2.reverse
+    val qPath = findNode(root, q, Nil)._2.reverse
+
+    pPath.zip(qPath).findLast(x => x._1 == x._2).get._1
+  }
+}
---
+++ src/test/scala/lowestCommonAncestorOfABinaryTree/SolutionTest.scala
@@ -0,0 +1,22 @@
+package lowestCommonAncestorOfABinaryTree
+
+import example.TreeNode
+import org.scalatest.flatspec.AnyFlatSpec
+
+class SolutionTest extends AnyFlatSpec {
+  val root = new TreeNode(
+    3,
+    new TreeNode(
+      5,
+      new TreeNode(6),
+      new TreeNode(2, new TreeNode(7), new TreeNode(4))
+    ),
+    new TreeNode(1, new TreeNode(0), new TreeNode(8))
+  )
+  "example 1" should "return 5" in {
+    val p = root.left
+    val q = root.right
+    val res = root
+    assert(Solution.lowestCommonAncestor(root, p, q) == res)
+  }
+}
