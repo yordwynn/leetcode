311a077e96a518de343c78274dd11c4678cd5180
yordwynn <yurij.a.egorov@gmail.com>
2022-08-04 23:41:19 +0500

    858. Mirror Reflection

---
+++ src/main/scala/mirrorReflection/Solution.scala
@@ -0,0 +1,22 @@
+package mirrorReflection
+
+import scala.annotation.tailrec
+
+object Solution {
+  def mirrorReflection(p: Int, q: Int): Int = {
+    @tailrec
+    def nok(res: Int, i: Int): (Int, Int) =
+      if (res % p == 0 && res % q == 0)
+        (res, i)
+      else
+        nok(res + q, i + 1)
+
+    val (n, i) = nok(q, 1)
+    if (i % 2 == 0)
+      2
+    else if (n % (p * 2) == 0)
+      0
+    else
+      1
+  }
+}
