a889b903ca55ad97b0da81a49cd20884f212ff3b
yordwynn <yurij.a.egorov@gmail.com>
2022-08-14 22:46:14 +0500

    126. Word Ladder II

--- src/test/scala/wordLadder2/SolutionTest.scala
+++ src/test/scala/wordLadder2/SolutionTest.scala
@@ -3,7 +3,7 @@ package wordLadder2
 import org.scalatest.flatspec.AnyFlatSpec
 
 class SolutionTest extends AnyFlatSpec {
-  "example 1" should "return [[hit,hot,dot,dog,dog],[hit,hot,lot,log,cog]]" in {
+  "example 1" should "return [[hit,hot,dot,dog,сog],[hit,hot,lot,log,cog]]" in {
     val beginWord = "hit"
     val endWord = "cog"
     val wordList = List("hot", "dot", "dog", "lot", "log", "cog")
