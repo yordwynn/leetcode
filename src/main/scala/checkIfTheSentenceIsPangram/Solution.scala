7ca4fa5a2304309918ee5f098650547949c1833b
yordwynn <yurij.a.egorov@gmail.com>
2022-10-18 23:50:42 +0600

    1832. Check if the Sentence Is Pangram

--- src/main/scala/checkIfTheSentenceIsPangram/Solution.scala
+++ src/main/scala/checkIfTheSentenceIsPangram/Solution.scala
@@ -3,9 +3,7 @@ package checkIfTheSentenceIsPangram
 // 1832. Check if the Sentence Is Pangram
 object Solution {
   def checkIfPangram(sentence: String): Boolean = {
-    val counter = sentence.foldLeft(0)((res, item) => {
-      res | 1 << (item - 'a')
-    })
+    val counter = sentence.foldLeft(0)((res, item) => res | 1 << (item - 'a'))
     counter == 67108863
   }
 }
