15e5525d19c31af78b0608872d8a9b72df9a5c7e
yordwynn <yurij.a.egorov@gmail.com>
2023-06-16 02:02:13 +0400

    300. Longest Increasing Subsequence

--- src/main/scala/longestIncreasingSubsequence/Solution.scala
+++ src/main/scala/longestIncreasingSubsequence/Solution.scala
@@ -8,17 +8,16 @@ object Solution {
     val dp = Array.fill(nums.length)(1)
 
     @tailrec
-    def go(i: Int = 1, res: Int = 1): Int = {
-      if (i >= nums.length)
-        res
-      else {
+    def go(i: Int = 1): Unit = {
+      if (i < nums.length) {
         for (j <- 0 until i) {
           if (nums(i) > nums(j)) dp(i) = dp(i).max(dp(j) + 1)
         }
-        go(i + 1, res.max(dp(i)))
+        go(i + 1)
       }
     }
 
     go()
+    dp.max
   }
 }
