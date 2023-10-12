470095333d6663e473b434594c85aa67936b9d1a
yordwynn <yurij.a.egorov@gmail.com>
2023-10-12 23:42:08 +0400

    // 102. Binary Tree Level Order Traversal

--- src/main/scala/binaryTreeLevelOrderTraversal/Solution.scala
+++ src/main/scala/binaryTreeLevelOrderTraversal/Solution.scala
@@ -9,31 +9,22 @@ import scala.collection.immutable.Queue
 object Solution {
   def levelOrder(root: TreeNode): List[List[Int]] = {
     @tailrec
-    def go(
-        nodes: Queue[(TreeNode, Int)],
-        levelNodes: List[Int] = List(),
+    def go2(
+        nodes: Vector[TreeNode],
         res: List[List[Int]] = List()
     ): List[List[Int]] = {
       if (nodes.isEmpty)
-        res.appended(levelNodes)
+        res
       else {
-        val (item, dequeued) = nodes.dequeue
-        item match {
-          case (node, level) =>
-            val addRight =
-              if (node.right != null) dequeued.enqueue((node.right, level + 1))
-              else dequeued
-            val addBoth =
-              if (node.left != null) addRight.enqueue((node.left, level + 1))
-              else addRight
-            if (level > res.length)
-              go(addBoth, List(node.value), res.appended(levelNodes))
-            else
-              go(addBoth, levelNodes.prepended(node.value), res)
-        }
+        val nextLevelNodes = for {
+          node <- nodes
+          nextNodes <- Vector(node.left, node.right).filter(_ != null)
+        } yield nextNodes
+        val currentNodes = nodes.map(_.value).toList
+        go2(nextLevelNodes, res.appended(currentNodes))
       }
     }
 
-    if (root != null) go(Queue((root, 0))) else Nil
+    go2(Vector(root).filter(_ != null))
   }
 }
